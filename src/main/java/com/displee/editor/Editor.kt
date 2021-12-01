de7188814e760e8cead6b6f91f921be04333b9bd
Matthew Toupin <matthewt100@hotmail.com>
2021-12-01 10:15:46 -0700

    Added app icon

--- src/main/java/com/displee/editor/Editor.kt
+++ src/main/java/com/displee/editor/Editor.kt
@@ -7,6 +7,12 @@ import javafx.fxml.Initializable
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
+import java.awt.AWTException
+import java.awt.Image
+import java.awt.SystemTray
+import java.awt.TrayIcon
+import java.io.IOException
+import javax.imageio.ImageIO
 
 lateinit var mainController: MainController
 
@@ -16,8 +22,43 @@ fun main(vararg args: String) {
 
 fun loadFXML(resource: String, controllerClass: Class<out Initializable>): Parent {
 	val loader = FXMLLoader(Editor::class.java.getResource(resource))
-	loader.setController(controllerClass.newInstance())
+	loader.setController(controllerClass.getDeclaredConstructor().newInstance())
 	return loader.load()
+}
+
+
+fun setIcons(stage: Stage) {
+	//checking for support
+	if (!SystemTray.isSupported()) {
+		println("System tray is not supported")
+		return
+	}
+
+	//get the systemTray of the system
+	val systemTray = SystemTray.getSystemTray()
+
+	//get default toolkit
+	//Toolkit toolkit = Toolkit.getDefaultToolkit();
+	//get image
+	//Toolkit.getDefaultToolkit().getImage("src/resources/busylogo.jpg");
+	var image: Image? = null
+	try {
+		image = ImageIO.read(DummyMain::class.java.getResourceAsStream("/images/icon.png"))
+		stage.icons.add(javafx.scene.image.Image(DummyMain::class.java.getResourceAsStream("/images/icon.png")))
+	} catch (e: IOException) {
+		e.printStackTrace()
+	}
+
+	//setting tray icon
+	val trayIcon = TrayIcon(image, "Cs2 Editor")
+
+	//adjust to default size as per system recommendation
+	trayIcon.isImageAutoSize = true
+	try {
+		systemTray.add(trayIcon)
+	} catch (awtException: AWTException) {
+		awtException.printStackTrace()
+	}
 }
 
 class Editor : Application() {
@@ -27,6 +68,7 @@ class Editor : Application() {
 		val loader = FXMLLoader(Editor::class.java.getResource("/fxml/main.fxml"))
 		loader.setController(mainController)
 		val root = loader.load<Parent>()
+
 		stage.scene = Scene(root, 1200.0, 800.0).also {
 			it.stylesheets.add("/css/theme.css")
 			it.stylesheets.add("/css/custom.css")
@@ -34,6 +76,7 @@ class Editor : Application() {
 			it.stylesheets.add("/css/code-area-ui.css")
 		}
 		stage.title = "Displee's CS2 editor"
+		setIcons(stage)
 		stage.show()
 	}
 
